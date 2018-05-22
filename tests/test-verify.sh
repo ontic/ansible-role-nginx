d2a9024b6a1cb855c1be45d77aa47b2a336a593c
Adam Dyson <adam.dyson@outlook.com>
2018-05-23 00:50:19 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
-# Verify the installed Nginx version.
+# Verify the installed location.
+docker exec --tty ${container_id} env TERM=xterm which nginx
+# Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm nginx -v
